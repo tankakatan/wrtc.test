2e5ef62e0446c137f829505b259e21b3737b1464
den <d.n.voropaev@gmail.com>
2020-10-23 00:32:34 +0300

    captured webcam video

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import ReactDom from 'react-dom'
 
+let width = 320
+let height = 0
+
+let streaming = false
+let video = null
+let canvas = null
+let photo = null
+let button = null
+
 const App = () => {
+    const video = useRef (null)
+
+    useEffect (() => {
+        void (async () => {
+
+            try {
+
+                const stream = await navigator.mediaDevices.getUserMedia ({ video: true, audio: false })
+                video.current.srcObject = stream
+                video.current.play ()
+
+            } catch (e) {
+
+                console.error ('User media error: ', e)
+            }
+
+        }) ()
+    }, [])
+
     return (
-        <div className='camera'>
-            <video id='video'>Video stream not available</video>
-            <a title='Take a shot' href='#'>Take a shot</a>
+        <div className='box'>
+            <div className='camera'>
+                <video ref={ video } id='video'>Video stream not available</video>
+                <a title='Take a shot' href='#'>Take a shot</a>
+            </div>
+            <canvas id='canvas'/>
+            <div id='output'>
+                <img id='image' alt='Screen shot will appear in thin box.'/>
+            </div>
         </div>
     )
 }
