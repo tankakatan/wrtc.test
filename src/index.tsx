a4374a6303ac5cacbdb5f1ceef938909f2d8780b
den <d.n.voropaev@gmail.com>
2020-10-23 00:56:42 +0300

    rendered a video screen shot into an image via the canvas

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react'
 import ReactDom from 'react-dom'
 
+const width = 320
+
 const App = () => {
 
     const video = useRef (null)
+    const image = useRef (null)
+    const canvas = useRef (null)
+
+    const [streaming, setStreaming] = useState (false)
 
     useEffect (() => {
         void (async () => {
@@ -14,9 +20,22 @@ const App = () => {
                 video.current.srcObject = stream
                 video.current.play ()
 
+                try {
+
+                    const context = canvas.current.getContext ('2d')
+                    context.fillStyle = '#AAA'
+                    context.fillRect (0, 0, canvas.current.width, canvas.current.height)
+
+                    image.current.setAttribute ('src', canvas.current.toDataURL ('image/png'))
+
+                } catch (e) {
+
+                    console.error ('Canvas error:', e)
+                }
+
             } catch (e) {
 
-                console.error ('User media error: ', e)
+                console.error ('User media error:', e)
             }
 
         }) ()
@@ -25,12 +44,42 @@ const App = () => {
     return (
         <div className='box'>
             <div className='camera'>
-                <video ref={ video } id='video'>Video stream not available</video>
-                <a title='Take a shot' href='#'>Take a shot</a>
+                <video
+                    ref={ video }
+                    id='video'
+                    onCanPlay={ () => {
+
+                        if (streaming === true) return
+
+                        const height = video.current.videoHeight / (video.current.videoWidth / width)
+
+                        video.current.setAttribute ('width', width)
+                        video.current.setAttribute ('height', height)
+                        canvas.current.setAttribute ('width', width)
+                        canvas.current.setAttribute ('height', height)
+
+                        setStreaming (true)
+                    } }
+                >
+                    Video stream not available
+                </video>
+                <a
+                    title='Take a shot'
+                    href='#'
+                    onClick={ () => {
+
+                        const context = canvas.current.getContext ('2d')
+                        context.drawImage (video.current, 0, 0, canvas.current.width, canvas.current.height)
+
+                        image.current.setAttribute ('src', canvas.current.toDataURL ('image/png'))
+                    } }
+                >
+                    Take a shot
+                </a>
             </div>
-            <canvas id='canvas'/>
+            <canvas ref={ canvas } id='canvas'/>
             <div id='output'>
-                <img id='image' alt='Screen shot will appear in thin box.'/>
+                <img ref={ image } id='image' alt='Screen shot will appear in thin box.'/>
             </div>
         </div>
     )
